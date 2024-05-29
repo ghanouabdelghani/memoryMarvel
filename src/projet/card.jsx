d793bd88f4c81661ea1f01853744b6c4ccbf8c0f
MacBook Pro <abdelghani.20112011@live.com>
2024-05-30 00:57:39 +0100

    first

--- src/projet/card.jsx
+++ src/projet/card.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import start from "../assets/icons/card-front.svg";
 
 export default function Card({ imge }) {
+  const [isFlipped, setIsFlipped] = useState(false);
   const [flipe, setflipe] = useState(start);
   return (
     <div>
